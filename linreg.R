ab300fe81d04747cd8a6c7138aac42dd4f9a6c29
Milda Poceviciute <m.poceviciute@gmail.com>
2017-09-13 16:17:01 +0200

    Linear regression code added to the lab4

--- linreg.R
+++ linreg.R
@@ -1,19 +1,44 @@
 
+
+
+
 # linreg ------------------------------------------------------------------
 data <- iris
-formula <- Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width
+formula <-
+    Sepal.Length ~ Species + Sepal.Width + Petal.Length + Petal.Width
 
-linreg <- function(formula, data){
+linreg <- function(formula, data) {
     stopifnot()
     #mf <- model.frame(formula, data)
-    mm <- model.matrix(formula, data)
-
-    y <- all.vars(expr = formula)[1]
+    X <- model.matrix(formula, data)
     
-    print(class(mm))
-    print(formula)
-    #return(mm)
+    # extract the dependant variable from data set
+    dep_name <- all.vars(expr = formula)[1]
+    y <- (data[, dep_name])
+    
+    # calculated the predicted values for beta parameter vector
+    beta_hat <- solve((t(X) %*% X)) %*% t(X) %*% y
+    
+    # find the fitted values of y using beta_hat
+    y_hat <- X %*% beta_hat
+    
+    # find the residual values
+    e_hat <- y - y_hat
+    
+    n <- nrow(X) #number of observations
+    p <- length(all.vars(formula)) - length(dep_name) #number of parameters
+    
+    # degrees of freedom
+    df <- n - p
+    
+    # residual variance
+    sigma_hat <- as.numeric((t(e_hat) %*% e_hat) / df)
+    var_beta_hat <- sigma_hat * solve((t(X) %*% X))
+    
+    #The t-values for each coefficient
+    t_value <- beta_hat / sqrt(diag(var_beta_hat))
+    p_value <- pt(t_value, df = df)
 }
 
-linreg(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, iris)
-
+# linreg(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width,
+#        iris)
