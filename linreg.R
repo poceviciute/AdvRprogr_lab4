c6573c4472d66ec9a684a6cb141ad497e88c981c
Henrik Karlsson <henkar91@gmail.com>
2017-09-14 14:27:37 +0200

    Save

--- linreg.R
+++ linreg.R
@@ -1,15 +1,10 @@
-
-
-
-
 # linreg ------------------------------------------------------------------
 data <- iris
-formula <-
-    Sepal.Length ~ Species + Sepal.Width + Petal.Length + Petal.Width
+formula <- Sepal.Length ~ Species + Sepal.Width + Petal.Length + Petal.Width
+
 
 linreg <- function(formula, data) {
-    stopifnot()
-    #mf <- model.frame(formula, data)
+    # Define matrix
     X <- model.matrix(formula, data)
     
     # extract the dependant variable from data set
@@ -18,6 +13,7 @@ linreg <- function(formula, data) {
     
     # calculated the predicted values for beta parameter vector
     beta_hat <- solve((t(X) %*% X)) %*% t(X) %*% y
+    # colnames(beta_hat) <- "Reg_Coef"
     
     # find the fitted values of y using beta_hat
     y_hat <- X %*% beta_hat
@@ -25,10 +21,10 @@ linreg <- function(formula, data) {
     # find the residual values
     e_hat <- y - y_hat
     
+    # degrees of freedom
     n <- nrow(X) #number of observations
     p <- length(all.vars(formula)) - length(dep_name) #number of parameters
     
-    # degrees of freedom
     df <- n - p
     
     # residual variance
@@ -38,7 +34,50 @@ linreg <- function(formula, data) {
     #The t-values for each coefficient
     t_value <- beta_hat / sqrt(diag(var_beta_hat))
     p_value <- pt(t_value, df = df)
+    
+    # Build RC-class
+    output <- setRefClass(
+        "linreg",
+        fields = list(
+            f_formula = "formula",
+            reg_coef = "matrix",
+            fitted_values = "numeric",
+            resid = "matrix",
+            df = "numeric",
+            var_resid = "numeric",
+            p_value = "matrix",
+            t_value = "matrix",
+            var_res_coef = "matrix"
+        ),
+        methods = list(
+            print.linreg <<- function(result) {
+                return(list(Formula_call = result$f_formula,
+                            Regression_Coefficients = result$reg_coef))
+            }
+            plot.linreg <<- function(result){
+                ggplot(data = result, aes(x = fitted_values, y = resid)) +
+                    geom_point()
+            }
+        )
+    )
+    
+    result <- output(
+        f_formula = formula,
+        reg_coef = beta_hat,
+        fitted_values = y,
+        resid = e_hat,
+        df = df,
+        var_resid = sigma_hat,
+        p_value = p_value,
+        t_value = t_value,
+        var_res_coef = var_beta_hat)
+    return(result)
 }
+eval <- linreg(formula, iris)
 
-# linreg(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width,
-#        iris)
+print(eval)
+plot(eval)
+
+library(ggplot2)
+ggplot(data = result, aes(x = fitted_values, y = resid)) +
+    geom_point()
